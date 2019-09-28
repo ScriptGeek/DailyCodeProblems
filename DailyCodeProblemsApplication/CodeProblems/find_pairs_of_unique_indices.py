c5604009fce1a7adaf1bbe779d7720ef5bdd8558
Jayson Ash <ash.jayson@gmail.com>
2019-09-27 18:15:45 -0700

    find all combos of palindromes within given list of strings

---
+++ DailyCodeProblemsApplication/CodeProblems/find_pairs_of_unique_indices.py
@@ -0,0 +1,47 @@
+"""
+Good morning! Here's your coding interview problem for today.
+
+This problem was asked by Airbnb.
+
+Given a list of words, find all pairs of unique indices such that the concatenation of the
+two words is a palindrome.
+
+For example, given the list ["code", "edoc", "da", "d"], return [(0, 1), (1, 0), (2, 3)].
+
+Upgrade to premium and get in-depth solutions to every problem, including this one.
+Since you were referred by one of our affiliates, you'll get a 10% discount on checkout!
+
+If you liked this problem, feel free to forward it along so they can subscribe here!
+As always, shoot us an email if there's anything we can help with!
+
+Ready to interview? Take Triplebyte's quiz and skip straight to final interviews with top tech companies!
+
+# ScriptGeek's solution:
+Iterate through all the combos O(n^2) loop in list of strings
+Don't check if both loops are at the same iteration value
+Concatenate the two strings and check if they are palindromes
+If palindrom is found add indices to a list
+Display the list when complete
+
+Interestingly, in the subject line of the email this problem was sent in it said it was Hard.
+I found this problem to be less difficult than the fibonacci problem.
+"""
+
+
+def findPairs(list):
+    results = []
+    for n in range(0, len(list)):
+        for m in range(0, len(list)):
+            if (n != m):
+                if (isPalindrome(list[n]+list[m])):
+                    results.append(("("+str(n)+", "+str(m)+")"))
+                    print (f"n: {n} m: {m}")
+
+def isPalindrome(string):
+    for n in range(0, len(str(string))//2):
+        if (string[n] != string[len(string) - 1 - n]):
+            return False
+    return True
+
+def runTest():
+    findPairs(["code", "edoc", "da", "d"])
--- DailyCodeProblemsApplication/DailyCodeProblemsApplication.py
+++ DailyCodeProblemsApplication/DailyCodeProblemsApplication.py
@@ -2,9 +2,11 @@ from CodeProblems import jump_to_the_end
 from CodeProblems import k_ary_symmetrical_tree_check
 from CodeProblems import smaller_to_the_right
 from CodeProblems import nth_fibonacci_number
+from CodeProblems import find_pairs_of_unique_indices
 
 #jump_to_the_end.runTest()
 #k_ary_symmetrical_tree_check.runTest()
 #smaller_to_the_right.runTest()
+#nth_fibonacci_number.runTest()
 
-nth_fibonacci_number.runTest()
+find_pairs_of_unique_indices.runTest()
